6a417936ccbe524467385ad62ea520d9e782f001
bickeymessi10-ux <bickeymessi10@gmail.com>
2026-09-03 21:42:56 +0545

    pattern for the tic tac toe game

--- script.js
+++ script.js
@@ -288,3 +288,18 @@ function handleTicClick(index) {
   ticCurrentPlayer = ticCurrentPlayer === 'X' ? 'O' : 'X';
   ticStatus.textContent = `Player ${ticCurrentPlayer}'s turn`;
 }
+function checkTicWinner() {
+  const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
+
+  for (const pattern of winPatterns) {
+    const [a, b, c] = pattern;
+    if (ticBoard[a] && ticBoard[a] === ticBoard[b] && ticBoard[a] === ticBoard[c]) {
+      return ticBoard[a];
+    }
+  }
+  return null;
+}
